36eaeab8644bbe312f8205b812a5e384df2bd97c
24DavidM <stalin.muela02@epn.edu.ec>
2025-08-31 14:12:45 -0500

    Exceptions

---
+++ src/basic/exception/ThrowExample.java
@@ -0,0 +1,20 @@
+package basic.exception;
+
+public class ThrowExample {
+    public void checkAge(int age) throws IllegalArgumentException {
+        if (age < 18) {
+            throw new IllegalArgumentException("Tienes que ser mayor de edad");
+        } else {
+            System.out.println("Es mayor de edad");
+        }
+    }
+
+    public void checkScore(int score) throws CustomException {
+        if (score < 0 || score > 100) {
+            throw new CustomException("Score invalido");
+        }else{
+            System.out.println("Puntuación válida: " + score);
+        }
+    }
+
+}
